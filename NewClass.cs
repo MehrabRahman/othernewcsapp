1143e2c21516e08e6f0e6e196ab50dcc83afa908
MehrabRahman <mehrab.rahman@revature.com>
2024-07-08 14:31:34 -0700

    My first other commit

---
+++ NewClass.cs
@@ -0,0 +1,10 @@
+namespace HelloWorldApp 
+{
+	class NewClass 
+	{
+		public static void Goodbye()
+		{
+			Console.WriteLine("Thanks for using the app!");
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorldApp 
+{
+
+	class App
+	{
+		static void Main(string[] args) 
+		{
+			Console.Write("Name = ");
+			var name = Console.ReadLine();
+			Console.WriteLine("Hello, " + name + "!");
+			Console.Write("How old are you? Age = ");
+			int age = int.Parse(Console.ReadLine());
+			Console.WriteLine("You are " + age);
+
+			NewClass.Goodbye();
+		}
+	}
+}
